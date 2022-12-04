ce3de35c5c63cc65fe881f60226becc2c89fba56
losier <arv95464@gmail.com>
2022-12-04 12:20:19 +0530

    added solution for question 11

--- src/11.c
+++ src/11.c
@@ -2,4 +2,21 @@
 
 int main()
 {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+    {
+        printf("%c is a vowel.", ch);
+    }
+    else if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+    {
+        printf("%c is a vowel.", ch);
+    }
+    else
+    {
+        printf("%c is a consonant.", ch);
+    }
 }
