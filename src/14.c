c4777483cf73046b837156343e750b6b0a7f36a2
losier <arv95464@gmail.com>
2022-12-04 12:32:02 +0530

    added solution for question 14

---
+++ src/14.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("Enter the year: ");
+    scanf("%d", &year);
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                printf("Leap year.");
+            }
+            else
+            {
+                printf("Not a leap year.");
+            }
+        }
+        else
+        {
+            printf("Leap year.");
+        }
+    }
+    else
+    {
+        printf("Not a leap year.");
+    }
+}
