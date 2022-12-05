b5ff3aa8c601a12d2349eb65ba52f711586f8deb
losier <arv95464@gmail.com>
2022-12-05 21:44:23 +0530

    added soln for question 21

---
+++ src/21.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, gcd;
+
+    printf("Enter the 1st number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the 2nd number: ");
+    scanf("%d", &num2);
+
+    for (int i = 1; i <= num1 && i <= num2; i++)
+    {
+        if (num1 % i == 0 && num2 % i == 0)
+        {
+            gcd = i;
+        }
+    }
+
+    printf("GCD of %d and %d is %d", num1, num2, gcd);
+}
